0332ce2b5ad3b12254e653ebc4bceb25a60c666a
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2023-04-25 09:32:27 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.32

---
+++ src/SpinTruncatedWigner.jl
@@ -0,0 +1,5 @@
+module SpinTruncatedWigner
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SpinTruncatedWigner
+using Test
+
+@testset "SpinTruncatedWigner.jl" begin
+    # Write your tests here.
+end
