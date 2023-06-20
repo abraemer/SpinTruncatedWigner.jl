e481f50ea6e0fb334a452262013b37d01369c6f4
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2023-06-20 08:49:52 +0200

    use FullSpecialize

--- src/SpinTruncatedWigner.jl
+++ src/SpinTruncatedWigner.jl
@@ -51,7 +51,7 @@ function TWAProblem end
 TWAProblem(H::SpinModels.Hamiltonian, ψ0, times) = TWAProblem(TWAParameters(H), ψ0, times)
 TWAProblem(H::dTWAParameters, ψ0::AbstractSpinState, times) = TWAProblem(H, classical(ψ0), times)
 function TWAProblem(param::dTWAParameters, ψ0::Matrix, times)
-    problem = ODEProblem(twaUpdate!, ψ0, (0, maximum(times)), param; saveat=times)
+    problem = ODEProblem{true, SciMLBase.FullSpecialize}(twaUpdate!, ψ0, (0, maximum(times)), param; saveat=times)
     ensemble = EnsembleProblem(problem;
 	    prob_func = (prob, i, repeat) -> remake(prob; u0 = twaSample(prob.u0)))
     return ensemble
@@ -62,7 +62,7 @@ TWAProblem(cb::ClusterBasis, H::SpinModels.Hamiltonian, ψ0, times) = TWAProblem
 TWAProblem(cb::ClusterBasis, H::cTWAParameters, ψ0::AbstractSpinState, times) = TWAProblem(cb, H, cTWAGaussianState(cb,ψ0), times)
 TWAProblem(cb::ClusterBasis, H::cTWAParameters, ψ0::Vector, times) = TWAProblem(cb, H, cTWAGaussianState(cb,ψ0), times)
 function TWAProblem(cb::ClusterBasis, param::cTWAParameters, ψ0::cTWAGaussianState, times)
-    problem = ODEProblem(twaUpdate!, ψ0, (0, maximum(times)), param; saveat=times)
+    problem = ODEProblem{true, SciMLBase.FullSpecialize}(twaUpdate!, ψ0, (0, maximum(times)), param; saveat=times)
     ensemble = EnsembleProblem(problem;
 	    prob_func = (prob, i, repeat) -> remake(prob; u0 = twaSample(prob.u0)))
     return ensemble
